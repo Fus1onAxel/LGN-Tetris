406ccf5989603f72b1dbab3fa00c72961b281401
ivanzarembovskyi-ship-it <ivanzarembovskyi@gmail.com>
2026-03-09 10:09:47 +0100

    Update MainWindow.axaml.cs

--- increment1/Tetris/InterfaceTetris/MainWindow.axaml.cs
+++ increment1/Tetris/InterfaceTetris/MainWindow.axaml.cs
@@ -112,8 +112,9 @@ public partial class MainWindow : Window
     */
     public void DessinerCarre(int x, int y, Avalonia.Media.IBrush couleur)
     {
+        //Les coordonnées sont conver
         //Création du carré qui correspond à l'outline
-        DessinerRectangle((x*22+12), y*22, 22, 22, IImmutableSolidColorBrush.Black);
+        DessinerRectangle((x*22+12), y*22, 22, 22, TranslateColor(1));
         //Remplissage du fond du carré par création d'un autre carré
         DessinerRectangle((x*22+12), y*22, 20, 20, couleur);
     }
