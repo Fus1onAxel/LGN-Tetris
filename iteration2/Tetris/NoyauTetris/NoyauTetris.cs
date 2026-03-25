24bd47eaa4d4c02f5a7faf8346e4bdf5ab05d4fd
ivanzarembovskyi-ship-it <ivanzarembovskyi@gmail.com>
2026-03-25 15:48:16 +0100

    Update NoyauTetris.cs

--- iteration2/Tetris/NoyauTetris/NoyauTetris.cs
+++ iteration2/Tetris/NoyauTetris/NoyauTetris.cs
@@ -1,8 +1,33 @@
-﻿namespace NoyauTetris;
+﻿namespace Tetris.NoyauTetris;
 
 public class NoyauTetris
 {
 
+}
+
+public class Tetrino
+{
+    public static Position[][] TetrinosTab = new Position[][]
+    {
+        // carre
+        new Position[] 
+        { 
+            new Position(0, 0), new Position(1, 0),
+            new Position(0, -1), new Position(1, -1) 
+        },
+        // barre horizontale
+        new Position[] 
+        { 
+            new Position(0, 0), new Position(1, 0),
+            new Position(2, 0), new Position(3, 0) 
+        },
+        // barre verticale
+        new Position[] 
+        { 
+            new Position(0, 0), new Position(0, -1),
+            new Position(0, -2), new Position(0, -3) 
+        }
+    }
 }
 
 public class JeuTetris
@@ -17,14 +42,54 @@ public class JeuTetris
     }
 }
 
-enum TetrinoCouleur
+/** Position
+    La classe position enregistre une position sur la grille
+    @author Axel DAVID
+    @param x la coordonnée horizontale dans l'espace dans la grille des carrés
+    @param y la coordonnée verticale dans l'espace dans la grille des carrés
+*/
+public class Position
 {
-    white, //absence de carré en blanc
-    black, //cadre et le tour des carrés en noir
-    blue,  //reste des couleurs pour les tetrinos
-    green,
-    red,
-    yellow,
-    violet,
-    orange
+    public int X;
+    public int Y;
+
+    public Position(int abs, int ord)
+    {
+        abs = X;
+        ord = Y;
+    }
+
+    public void Gauche()
+    {
+        if(X > 0)
+        {
+            X = X - 1;
+        }
+    }
+    public void Droite()
+    {
+        if(X < 11)
+        {
+            X = X + 1;
+        }
+    }
+    public void Bas()
+    {
+        if(Y < 14)
+        {
+            Y = Y - 1;
+        }
+    }
+}
+
+public enum TetrinoCouleur
+{
+    white = 0, //absence de carré en blanc
+    black = 1, //cadre et le tour des carrés en noir
+    blue = 2,  //reste des couleurs pour les tetrinos
+    green = 3,
+    red = 4,
+    yellow = 5,
+    violet = 6,
+    orange = 7
 }
