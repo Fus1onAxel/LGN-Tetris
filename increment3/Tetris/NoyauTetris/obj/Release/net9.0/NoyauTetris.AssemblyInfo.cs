2f41ce77b1b0cc6b49db98b6837fa54179f8d0a4
Axel DAVID <axeld41500@gmail.com>
2026-04-22 14:07:54 +0200

    -

--- increment3/Tetris/NoyauTetris/obj/Release/net9.0/NoyauTetris.AssemblyInfo.cs
+++ increment3/Tetris/NoyauTetris/obj/Release/net9.0/NoyauTetris.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NoyauTetris")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dca3eda615817194b776e84e6cec2362860b5101")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c637c04177387c1ae4f7b0777902a7f659558a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("NoyauTetris")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NoyauTetris")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
